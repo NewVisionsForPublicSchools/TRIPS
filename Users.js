4d898e78000540fa458a4dbb4f710b049e052653
coreyjbrown <coreyjbrown@gmail.com>
2015-07-29 16:18:17 -0400

    Add user validation

---
+++ Users.js
@@ -0,0 +1,16 @@
+var dbString = '//ga-sql-bootcamp:demodb0615/TRIPS';
+var userColumns = 'username,roles';
+
+
+
+function validateUser(){
+  var test, currentUser, query, validUser;
+
+  currentUser = Session.getActiveUser().getEmail();
+//  currentUser = 'approver1@charter.newvisions.org';
+  query = 'SELECT * FROM users WHERE username = "' + currentUser + '"';
+  
+  validUser = NVGAS.getSqlRecords(dbString, query)[0] ? NVGAS.getSqlRecords(dbString, query)[0].username : "";
+
+  return validUser ? true : false;
+}
