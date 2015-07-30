536e701920b5bce4712d63a120d9af220a5ef88d
coreyjbrown <coreyjbrown@gmail.com>
2015-07-29 23:40:35 -0400

    Add AP Approval

--- Processing.js
+++ Processing.js
@@ -73,3 +73,79 @@ function loadNewReqForm(trip_id){
   html.approver = Session.getActiveUser().getEmail();
   return html.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME).getContent();
 }
+
+
+
+function processNewTrpApproval(formObj){
+  var test, queryArray, rQuery, aQuery, html, approverAlert, newQueue;
+  
+  queryArray = [];
+  rQuery = 'UPDATE tracking t SET t.status = "' + formObj.status
+           + '", t.justification = "' + formObj.justification + '", t.approver = "' + formObj.approver
+           + '" WHERE t.trip_id = "' + formObj.trip_id + '"';
+  queryArray.push(rQuery);
+  
+  switch(formObj.status){
+    case 'Approved':
+      aQuery = 'UPDATE tracking SET ap_approval = "' + new Date() + '" WHERE trip_id = "' + formObj.trip_id + '"';
+      queryArray.push(aQuery);
+      break;
+      
+    case 'Denied':
+      aQuery = 'UPDATE tracking SET denial = "' + new Date() + '", queue = "" WHERE trip_id = "' + formObj.trip_id + '"';
+      queryArray.push(aQuery);
+      break;
+      
+    default:
+      break;
+  }
+ 
+  NVGAS.insertSqlRecord(dbString, queryArray);
+  checkApprovalStatus(formObj);
+  
+  html = HtmlService.createTemplateFromFile('process_new_trip_confirmation');
+  html.request = formObj.trip_id;
+  return html.evaluate().setSandboxMode(HtmlService.SandboxMode.IFRAME).getContent();
+}
+
+
+
+function checkApprovalStatus(approvalObj){
+//  var test, status, trip;
+//  
+//  status = approvalObj.status;
+//  trip = approvalObj.trip_id;
+//  
+//  switch (status){
+//    case 'Approved':
+//      sendApprovalEmail(request);
+//      break;
+//      
+//    case 'Denied':
+//      sendDenialEmail(request);
+//      break;
+//      
+//    default:
+//      break;
+//  } 
+}
+
+
+
+function sendApprovalEmail(trip_id){
+  var test, trip, recipient, subject, html, template, ccQuery, copyList;
+  
+  trip = getTrip(trip_id);
+  recipient = trip.username;
+  subject = "DO NOT REPLY: Supply Request Approved | " + request.request_id;
+  html = HtmlService.createTemplateFromFile('approval_email');
+  html.request = request;
+  template = html.evaluate().getContent();
+  ccQuery = 'SELECT username FROM users WHERE roles LIKE "%DSO%" OR roles LIKE "%BM%"';
+  copyList = NVGAS.getSqlRecords(dbString, ccQuery).map(function(e){
+    return e.username;
+  }).join();
+  
+  GmailApp.sendEmail(recipient, subject,"",{htmlBody: template,
+                                            cc: copyList});
+}
